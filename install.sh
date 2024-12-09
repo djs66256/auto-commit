2dfc63049fbd23b3a6254f25ffd26cb0022d4282
daniel <djs66256@163.com>
2024-12-09 15:31:21 +0800

    feat: 升级 pip 安装依赖，并在 `~/.bash_profile` 中添加别名

--- install.sh
+++ install.sh
@@ -3,8 +3,9 @@ cd ~
 git clone https://github.com/djs66256/auto-commit.git
 
 cd auto-commit
-pip install -r requirements.txt
+pip3 install --upgrade -r requirements.txt
 
 echo "alias gca='python3 ~/auto-commit/commit.sh -a'" >> ~/.bashrc
+echo "alias gca='python3 ~/auto-commit/commit.sh -a'" >> ~/.bash_profile
 
 echo "DONE!"
