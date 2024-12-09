f5ca31023def84b0eefa07695271f73d35f6c664
daniel <djs66256@163.com>
2024-12-09 15:36:41 +0800

    feat: 更新 `install.sh` 脚本，添加自动安装依赖和设置别名的功能。

--- install.sh
+++ install.sh
@@ -1,11 +1,43 @@
 cd ~
 
+echo "Download auto-commit..."
 git clone https://github.com/djs66256/auto-commit.git
 
+if pip3 install -q --upgrade pip; then
+    echo "Pip3 is installed."
+else
+    echo "Pip3 is not installed."
+    exit 1
+fi
+
+if python3 -m pip install --upgrade pip; then
+    echo "Python3 is installed."
+else
+    echo "Python3 is not installed."
+    exit 1
+fi
+
+echo "Install requirements..."
 cd auto-commit
 pip3 install --upgrade -r requirements.txt
 
-echo "alias gca='python3 ~/auto-commit/commit.sh -a'" >> ~/.bashrc
-echo "alias gca='python3 ~/auto-commit/commit.sh -a'" >> ~/.bash_profile
+echo "Setup alias..."
+# if bashrc exists, install to bashrc, otherwise install to bash_profile
+if [ -f ~/.bashrc ]; then
+    bashrc_path=~/.bashrc
+else
+    echo "bashrc does not exist."
+    if [ -f ~/.bash_profile ]; then
+        echo "bash_profile exists."
+        bashrc_path=~/.bash_profile
+    else
+        echo "bash_profile does not exist."
+        exit 1
+    fi
+fi
+
+echo "alias gca='python3 ~/auto-commit/commit.sh -a'" >> $bashrc_path
+
+source $bashrc_path
 
 echo "DONE!"
