08e1d4f6102f388df3d468f6b0bbd654da109ed7
daniel <djs66256@163.com>
2024-12-29 11:29:52 +0800

    feat: 添加了支持添加别名的功能

--- install.sh
+++ install.sh
@@ -4,11 +4,13 @@
 # 初始化变量
 provider="ollama"
 token="ollama"
+add_alias=false
 
 usage() {
-    echo "Usage:  [-p provider] [-t token] -h"
+    echo "Usage:  [-p provider] [-t token] -s -h"
     echo "  -p provider: llm provider name, ollama/chatglm/deepseek"
     echo "  -t token: api_key, you need an api_key to access the provider"
+    echo "  -s add alias gca to bashrc"
     echo "  -h help"
     exit 0
 }
@@ -19,6 +21,8 @@ while getopts "p:t:h" opt; do
         p) provider="$OPTARG"
            ;;
         t) token="$OPTARG"
+           ;;
+        s) add_alias=true
            ;;
         h) usage
            ;;
@@ -51,25 +55,27 @@ cd auto-commit
 pip3 install --upgrade -r requirements.txt
 
 echo "Setup config..."
-echo "settings:\n  provider: ${provider}\n  api_key: ${provider}\n" > ~/.auto-commit/config.yaml
+echo "settings:\n  provider: ${provider}\n\n${provider}:\n  api_key: ${token}\n" > ~/.auto-commit/config.yaml
 
+if [ $add_alias == true ]; then
 echo "Setup alias..."
-# if bashrc exists, install to bashrc, otherwise install to bash_profile
-if [ -f ~/.bashrc ]; then
-    bashrc_path=~/.bashrc
-else
-    echo "bashrc does not exist."
-    if [ -f ~/.bash_profile ]; then
-        echo "bash_profile exists."
-        bashrc_path=~/.bash_profile
+    # if bashrc exists, install to bashrc, otherwise install to bash_profile
+    if [ -f ~/.bashrc ]; then
+        bashrc_path=~/.bashrc
     else
-        echo "bash_profile does not exist."
-        exit 1
+        echo "bashrc does not exist."
+        if [ -f ~/.bash_profile ]; then
+            echo "bash_profile exists."
+            bashrc_path=~/.bash_profile
+        else
+            echo "bash_profile does not exist."
+            exit 1
+        fi
     fi
+
+    echo "alias gca='python3 ~/auto-commit/commit.py -a'" >> $bashrc_path
+
+    source $bashrc_path
 fi
-
-echo "alias gca='python3 ~/auto-commit/commit.py -a'" >> $bashrc_path
-
-source $bashrc_path
 
 echo "DONE!"
