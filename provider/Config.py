c50659db07ad2ffe5c945bd75954d10365c33526
daniel <djs66256@163.com>
2025-01-04 17:59:01 +0800

    feat: Refactor configuration file handling and improve config merging logic

--- provider/Config.py
+++ provider/Config.py
@@ -15,19 +15,21 @@ class ProviderConfig:
         if template is not None:
             return Template(template).render(**kwargs)
         
-class GlobalConfig:
-    DEFUALT_FILE = f"{os.path.dirname(__file__)}/../config.yml"
-    USER_FILE = f'{os.getenv("HOME")}/.auto-commit/config.yml'
-    
-    def __init__(self):
-        with open(self.DEFUALT_FILE) as f:
-            self.default_config = yaml.load(f, Loader=yaml.FullLoader)
-        if os.path.exists(self.USER_FILE):
-            with open(self.USER_FILE) as f:
-                self.user_config = yaml.load(f, Loader=yaml.FullLoader)
-        else:
-            self.user_config = {}
 
+def get_default_file_path(ex_config = None):
+    if ex_config:
+        return f"{os.path.dirname(__file__)}/../config.{ex_config}.yml"
+    else:
+        return f"{os.path.dirname(__file__)}/../config.yml"
+
+def get_user_config_file_path(ex_config = None):
+    if ex_config:
+        return f'{os.getenv("HOME")}/.auto-commit/config.{ex_config}.yml'
+    else:
+        return f'{os.getenv("HOME")}/.auto-commit/config.yml'
+    
+
+class GlobalConfig:
     def getExConfig(self, ex_config):
         path = f"{os.path.dirname(__file__)}/../config.{ex_config}.yml"
         if os.path.exists(path):
@@ -35,15 +37,39 @@ class GlobalConfig:
                 return yaml.load(f, Loader=yaml.FullLoader)
         return None
     
+    def getConfigFileList(self, ex_config = None):
+        if ex_config is None:
+            return [
+                get_default_file_path(),
+                get_user_config_file_path(),
+            ]
+        else:
+            return [
+                get_default_file_path(),
+                get_default_file_path(ex_config),
+                get_user_config_file_path(),
+                get_user_config_file_path(ex_config)
+            ]
+    
+    def getConfigInFiles(self, files, key):
+        result = {}
+        for file in files:
+            if os.path.exists(file):
+                with open(file) as f:
+                    config = yaml.load(f, Loader=yaml.FullLoader)
+                    value = config.get(key)
+                    if value:
+                        result = {**result, **value}
+        return result
+
+    def _getConfig(self, key, ex_config = None):
+        files = self.getConfigFileList(ex_config)
+        return self.getConfigInFiles(files, key)
+    
     def getConfig(self, provider, ex_config = None):
-        base_user_config = self.user_config.get("base") or {}
-        base_default_config = self.default_config.get("base") or {}
-        base_ex_config = {}
-        if ex_config:
-            base_ex_config = self.getExConfig(ex_config) or {}
-        user_config = self.user_config.get(provider) or {}
-        default_config = self.default_config.get(provider) or {}
-        merged_config = {**base_default_config, **base_user_config, **base_ex_config, **default_config, **user_config}
+        base_config = self._getConfig("base", ex_config)
+        provider_config = self._getConfig(provider, ex_config)
+        merged_config = {**base_config, **provider_config}
         return ProviderConfig(provider, merged_config)
 
 if __name__ == "__main__":
